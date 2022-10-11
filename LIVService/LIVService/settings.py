fa481ca9a2fafeaf3993404e75462c848bd0b936
mwenvinicius <viniciusdias2b@gmail.com>
2022-10-11 16:05:48 -0300

    #02 Correction.

--- LIVService/LIVService/settings.py
+++ LIVService/LIVService/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!^anhrm1un7a&m(kx2os690fozw)qgrs@%s!wp)sg9=(bh)!eo'
+SECRET_KEY = '-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
