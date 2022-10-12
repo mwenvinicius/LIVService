6d5eb5e62d39860d24c53123cdc727b73c3c4084
mwenvinicius <viniciusdias2b@gmail.com>
2022-10-12 12:40:06 -0300

    #03 Configuring Views, URL's and Templates.

--- LIVService/appservices/views.py
+++ LIVService/appservices/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def services(request):
+    context = {
+        'mensagem':'Olá, tudo bem? Você está na página de serviços.'
+    }
+    return render(request, 'services/services.html', context)
+
+def service(request, pk):
+    return render(request, 'services/single-service.html')
